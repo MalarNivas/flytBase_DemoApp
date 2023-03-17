bdc9170bbeba64870316b8d281fa3c4b95c53e04
MalarNivas <128149392+MalarNivas@users.noreply.github.com>
2023-03-17 17:48:53 +0530

    Assessment

---
+++ triangle_trajectory.py
@@ -0,0 +1,20 @@
+import time
+from flyt_python import api
+drone = api.navigation(timeout=120000) # instance of flyt droneigation class
+
+time.sleep(3)
+
+print 'taking off'
+drone.take_off(10.0)
+
+print ' going along the setpoints'
+drone.position_set(10,0,0,relative=True)
+drone.position_set(-5,10,0,relative=True)
+drone.position_set(-5,-10,0,relative=True)
+
+
+print 'Landing'
+drone.land(async=False)
+
+#shutdown the instance
+drone.disconnect()
